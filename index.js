d2da4ef9605f73d864a5d6d2318261d6bd3d88dd
Julian Brown <julianxbrown@gmail.com>
2018-09-10 01:49:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Add your functions and code here
+var kittens = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendKitten(name) {
+kittens.push(name);
+}
+  
+function destructivelyPrependKitten(name) {
+kittens.unshift("Bob");
+}
+
+function destructivelyRemoveLastKitten() {
+kittens.pop();
+  }
+
+function destructivelyRemoveFirstKitten() {
+kittens.shift();
+}
+
+function appendKitten(name) {
+return [name,...kittens];
+}
+
+function prependKitten(name) {
+return [name,...kittens];  
+}
+
+function removeLastKitten() {
+
+}
+
+function removeFirstKitten() {
+return kittens.slice(1);
+}
